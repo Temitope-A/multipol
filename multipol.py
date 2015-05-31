fab247aea1ee390246b1489142e4efdb0ee84c11
Temitope-A <temitope.a@live.it>
2015-05-31 12:56:09 +0100

    Version 0.1

--- multipol.py
+++ multipol.py
@@ -2,6 +2,7 @@ from numbers import Number, Integral
 import re
 import json
 
+
 class MultiPol(object):
 # Constructor method
 # Lowest degrees first
@@ -9,13 +10,13 @@ class MultiPol(object):
 
         MultiPol.clean(pol_list)
         self.pol = MultiPol.normalize(pol_list)
-        #TO DO: Check that the lists ultimately contain 
-        #Numbers  
-    
+        #TO DO: Check that the lists ultimately contain
+        #Numbers
+
         self.poldegree = len(self.pol) - 1
         self.degree = MultiPol.caldegree(self.pol)
-    
-        if var == None:
+
+        if var is None:
             self.var = MultiPol.calcvar(self.pol)
         else:
             try:
@@ -31,11 +32,11 @@ class MultiPol(object):
 
 #Evaluation by call
     def __call__(self, value):
-        return self.evaluate(value)                
+        return self.evaluate(value)
 
 ###### Operator methods #######################################################
 
-#Equality checking.  
+#Equality checking.
 
     def __eq__(self, other):
         if isinstance(other, Number):
@@ -45,7 +46,7 @@ class MultiPol(object):
             if self.poldegree != d:
                 return False
             else:
-                for i in range (0, d + 1):
+                for i in range(0, d + 1):
                     a = self.pol[i]
                     b = other.pol[i]
 
@@ -62,15 +63,14 @@ class MultiPol(object):
                             return MultiPol(a) == MultiPol(b)
             return True
 
-
-#Addition and subtraction
+#Addition and subtraction (addition needs amending)
 
     def __add__(self, other):
         try:
             d = other.poldegree
         except AttributeError:
             if isinstance(other, Number):
-                return self + Multipol([other])
+                return self + MultiPol([other])
             else:
                 raise TypeError(other)
         else:
@@ -81,29 +81,29 @@ class MultiPol(object):
                 b = other.pol[i]
                 if isinstance(a, Number):
                     if isinstance(b, Number):
-                        pol_list.append(a+b)
+                        pol_list.append(a + b)
                     else:
                         pol_list.append((MultiPol([a]) + MultiPol(b)).pol)
                 elif isinstance(b, Number):
                     pol_list.append((MultiPol(a) + MultiPol([b])).pol)
                 else:
-                    pol_list.append((MultiPol(a)+MultiPol(b)).pol)
+                    pol_list.append((MultiPol(a) + MultiPol(b)).pol)
 
                 pol_list.extend(self.pol[k:])
                 pol_list.extend(other.pol[k:])
 
         return MultiPol(pol_list, max(self.var, other.var))
-    
+
     def __radd__(self, other):
         return self + other
 
     def __neg__(self):
         pol_list = []
-        for i in range(0, self.poldegree+1):
-           if isinstance(self.pol[i], Number):
-               pol_list.append(-self.pol[i])
-           else:
-               pol_list.append((-MultiPol(self.pol[i])).pol)
+        for i in range(0, self.poldegree + 1):
+            if isinstance(self.pol[i], Number):
+                pol_list.append(-self.pol[i])
+            else:
+                pol_list.append((-MultiPol(self.pol[i])).pol)
 
         return MultiPol(pol_list, self.var)
 
@@ -120,26 +120,29 @@ class MultiPol(object):
             if isinstance(other, Number):
                 return self * MultiPol([other])
             else:
-                raise TypeError('{} Is not a polynomial or number'.format(other))
+                raise TypeError('{} Is not a polynomial '
+                                'or number'.format(other))
         else:
-            pol_list = [0 for i in range(0, m+1)]
+            pol_list = [0 for i in range(0, m + 1)]
             for i in range(0, self.poldegree + 1):
                 for j in range(0, other.poldegree + 1):
                     a = self.pol[i]
                     b = other.pol[j]
-                    if (isinstance(a, Number) and isinstance(b, Number) and isinstance(pol_list[i + j], Number)):
-                        pol_list[i + j] += a*b
+                    if (isinstance(a, Number) and isinstance(b, Number)
+                    and isinstance(pol_list[i + j], Number)):
+                        pol_list[i + j] += a * b
                     else:
                         if isinstance(a, Number):
                             a = [a]
                         if isinstance(b, Number):
                             b = [b]
-                        if isinstance(pol_list[i + j], Number):       
+                        if isinstance(pol_list[i + j], Number):
                             pol_list[i + j] = [pol_list[i + j]]
 
-                        x = MultiPol(a,d) * MultiPol(b,d) + MultiPol(pol_list[i + j],d)
+                        x = (MultiPol(a, d) * MultiPol(b, d)
+                             + MultiPol(pol_list[i + j], d))
                         pol_list[i + j] = x.pol
-        
+
         return MultiPol(pol_list, d)
 
     def __rmul__(self, other):
@@ -153,7 +156,7 @@ class MultiPol(object):
         elif not(isinstance(exp, Integral) and (exp > 0)):
             raise TypeError(exp)
         else:
-            return self*self**(exp-1)
+            return self * self ** (exp - 1)
 
 #-Division
 #    def __truediv__(self, other):
@@ -164,7 +167,6 @@ class MultiPol(object):
 #            if isinstance(other, Number):
 #                return self/Polynomial([other])
 #            else:
-
 
 # Evaluation via Horner's method ##############################################
 
@@ -178,17 +180,18 @@ class MultiPol(object):
 
         else:
             if len(x) < self.var:
-                raise IndexError('Variables and points of evaluation not compatible')
+                raise IndexError('Variables and points of evaluation'
+                                 ' not compatible')
             else:
-                return MultiPol.__evaluate(self.pol,x)
+                return MultiPol.__evaluate(self.pol, x)
 
     def __evaluate(pol_list, x):
 
         if isinstance(pol_list, Number):
             return pol_list
         else:
-            res = MultiPol.__evaluate(pol_list[-1],x[:-1])
-            for i in reversed(range(0, len(pol_list)-1)):
+            res = MultiPol.__evaluate(pol_list[-1], x[:-1])
+            for i in reversed(range(0, len(pol_list) - 1)):
                 res = x[-1] * res + MultiPol.__evaluate(pol_list[i], x[:-1])
 
             return res
@@ -216,8 +219,8 @@ class MultiPol(object):
                     else:
                         break
                 else:
-                    if MultiPol(pol_list[-1]) == 0: #This eliminates      
-                        pol_list.pop()              #trailing zeroes
+                    if MultiPol(pol_list[-1]) == 0:  # This eliminates
+                        pol_list.pop()               # trailing zeroes
                     else:
                         break
                 finally:
@@ -228,17 +231,17 @@ class MultiPol(object):
 
     def normalize(pol_list):
         s = str(pol_list)
-        match = re.search('\[\[.\]\]',s)
-        while match != None:
+        match = re.search('\[\[.\]\]', s)
+        while match is not None:
             s = re.sub('\[\[.\]\]', MultiPol.change, s)
-            match = re.search('\[\[.\]\]',s)
+            match = re.search('\[\[.\]\]', s)
 
         return json.loads(s)
 
     def change(matchobj):
 
-        x = matchobj.group(0).replace('[[','[')
-        return x.replace(']]',']')
+        x = matchobj.group(0).replace('[[', '[')
+        return x.replace(']]', ']')
 
 # This method computes the degree of a multivariable Polynomial as max degree of
 # the monomials.
@@ -251,7 +254,7 @@ class MultiPol(object):
             else:
                 print(Err(pol_list))
         else:
-            return max([MultiPol.caldegree(x) + pol_list.index(x)\
+            return max([MultiPol.caldegree(x) + pol_list.index(x)
                        for x in pol_list])
 
 # This method computes the number of variables
@@ -270,7 +273,7 @@ class MultiPol(object):
                 else:
                     return 1 + MultiPol.calcvar(pol_list[0])
             else:
-                return 1 +max([MultiPol.calcvar(x) for x in pol_list])                
+                return 1 + max([MultiPol.calcvar(x) for x in pol_list])
 
 #Representation methods #######################################################
 #__repr__ returns the formal representation,
--- str2pol.py
+++ str2pol.py
@@ -6,60 +6,62 @@
 
 from multipol import MultiPol
 
+
 def str2pol(strpol, var):
     varnum = len(var)
 
-    toadd=strpol.split('+')
-    if len(toadd)>1:     
-        poly=MultiPol([0])
+    toadd = strpol.split('+')
+    if len(toadd) > 1:
+        poly = MultiPol([0])
         for elem in toadd:
-            poly = poly+str2pol(elem, var)
+            poly = poly + str2pol(elem, var)
         return poly
     else:
         tosub = strpol.split('-')
-        if len(tosub)>1:
-            if tosub[0]=='':
-                poly=MultiPol([0])
+        if len(tosub) > 1:
+            if tosub[0] == '':
+                poly = MultiPol([0])
             else:
-                poly=str2pol(tosub[0],var)
-            for i in range(1,len(tosub)):
-                poly=poly - str2pol(tosub[i],var)
+                poly = str2pol(tosub[0], var)
+            for i in range(1, len(tosub)):
+                poly = poly - str2pol(tosub[i], var)
             return poly
         else:
-            tomult=strpol.split('*')
-            if len(tomult)>1:
-                poly=MultiPol([1])
+            tomult = strpol.split('*')
+            if len(tomult) > 1:
+                poly = MultiPol([1])
                 for elem in tomult:
-                    poly=poly*str2pol(elem, var)
+                    poly = poly * str2pol(elem, var)
                 return poly
             else:
-                topow=strpol.split('^')
-                if len(topow)>2:
-                    raise SyntaxError('Not a valid string for a polynomial')
-                elif len(topow)==2:
+                topow = strpol.split('^')
+                if len(topow) > 2:
+                    raise SyntaxError('Invalid string')
+                elif len(topow) == 2:
                     try:
-                        x=float(topow[1])
-                        d=int(x)
+                        x = float(topow[1])
+                        d = int(x)
                     except:
-                        raise SyntaxError('Not a valid string for a polynomial')
+                        raise SyntaxError('Invalid string')
                     else:
                         if x != d:
-                            raise SyntaxError('Not a valid string for a polynomial')
-                    return str2pol(topow[0],var)**d
+                            raise SyntaxError('Invalid string')
+                    return str2pol(topow[0], var) ** d
                 else:
                     if isfloat(strpol):
-                        if int(strpol)==float(strpol):
+                        if int(strpol) == float(strpol):
                             return MultiPol([int(strpol)])
                         else:
                             return MultiPol([float(strpol)])
 
-                    for i in range(0,varnum):
-                        if strpol==var[i]:
-                            v = [0,1]
-                            for j in reversed(range(i,varnum-1)):
+                    for i in range(0, varnum):
+                        if strpol == var[i]:
+                            v = [0, 1]
+                            for j in reversed(range(i, varnum - 1)):
                                 v = [v]
                             return MultiPol(v)
-                    raise SyntaxError('Not a valid string for a polynomial')
+                    raise SyntaxError('Invalid string')
+
 
 def isfloat(value):
     try:
