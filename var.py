3ed37ecc1a499fe86a8051eaaa6bf83deb17d48c
Temitope Ajileye <temitope.a@live.it>
2015-05-31 14:58:25 +0100

    added standar variables

---
+++ var.py
@@ -0,0 +1,9 @@
+#Import the variables into interactive mode or into a script and construct polynomials with python standard operators.
+
+from multipol import MultiPol as pol
+
+x = pol([0,1],1)
+
+y = pol([0,1],2)
+
+z = pol([0,1],3)
